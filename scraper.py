628662bf7894bc741b183846cfe800d688acae04
RdotSilva <RyandotSilva@gmail.com>
2020-04-15 19:31:59 -0400

    Add check_stock_crc_vrs method

--- scraper.py
+++ scraper.py
@@ -15,9 +15,21 @@ class Scraper(object):
     def load_crc_vrs_url(self, url):
         self.driver.get(url)
 
-        # Use a wait to make sure the page is ready to e scraped
+        # Use a wait to make sure the page is ready to be scraped
         try:
             wait = WebDriverWait(self.driver, self.delay)
             wait.until(EC.presence_of_element_located((By.ID, "crcPDP1")))
         except TimeoutException:
             print("URL took too long to load")
+
+    # Check the page to see if the bike is in stock
+    def check_stock_crc_vrs(self):
+        # Wait for Accept Cookie Button to be present. Without clicking this button you can't scrape the page.
+        # This button will pop up every time we reload the page.
+        try:
+            wait = WebDriverWait(self.driver, self.delay)
+            wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, "crc_accept_cookie"))
+            )
+        except TimeoutException:
+            print("Accept Cookie Button took too long to load")
