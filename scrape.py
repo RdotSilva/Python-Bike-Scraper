e42cd05380f8a7596a8d8664433fa6259e56e50e
RdotSilva <RyandotSilva@gmail.com>
2020-04-15 21:14:47 -0400

    Add scrape sequence

--- scrape.py
+++ scrape.py
@@ -1,3 +1,10 @@
 from scraper import Scraper
 
 crc_vrs_url = "https://www.chainreactioncycles.com/us/en/vitus-nucleus-29-vrs-bike-deore-1x10-2020/rp-prod181496"
+
+# Create new instance of scraper
+crc_vrs_scraper = Scraper()
+
+# Load crc vrs url and  check for stock
+crc_vrs_scraper.load_crc_vrs_url(crc_vrs_url)
+crc_vrs_scraper.check_stock_crc_vrs()
