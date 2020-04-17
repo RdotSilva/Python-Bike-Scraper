07268875c8f2e7552dc2b4083fa17d14255bc692
RdotSilva <RyandotSilva@gmail.com>
2020-04-16 21:18:09 -0400

    Add support for wiggle_vrs scraping

--- scrape.py
+++ scrape.py
@@ -1,11 +1,19 @@
 from scraper import Scraper
 
 crc_vrs_url = "https://www.chainreactioncycles.com/us/en/vitus-nucleus-29-vrs-bike-deore-1x10-2020/rp-prod181496"
+wiggle_vrs_url = "https://www.wiggle.com/vitus-nucleus-29-vrs-bike-deore-1x10-2020"
+
 
 # Create new instance of scraper
 crc_vrs_scraper = Scraper()
+wiggle_vrs_scraper = Scraper()
 
 # Load crc vrs url and  check for stock
 crc_vrs_scraper.load_crc_vrs_url(crc_vrs_url)
 crc_vrs_scraper.check_stock_crc_vrs()
 crc_vrs_scraper.quit()
+
+# Load Wiggle VRS url and check for stock
+wiggle_vrs_scraper.load_url_wiggle_vrs(wiggle_vrs_url)
+wiggle_vrs_scraper.check_stock_wiggle_vrs()
+wiggle_vrs_scraper.quit()
