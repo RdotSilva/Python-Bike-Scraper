234b18cde5b458948715a05574875913e3d17784
RdotSilva <RyandotSilva@gmail.com>
2020-04-17 21:45:33 -0400

    Add crc_vr_url

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from scraper import Scraper
 
 crc_vrs_url = "https://www.chainreactioncycles.com/us/en/vitus-nucleus-29-vrs-bike-deore-1x10-2020/rp-prod181496"
+crc_vr_url = "https://www.chainreactioncycles.com/us/en/vitus-nucleus-29-vr-bike-altus-2x9-2020/rp-prod181494"
 wiggle_vrs_url = "https://www.wiggle.com/vitus-nucleus-29-vrs-bike-deore-1x10-2020"
 
 
